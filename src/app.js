46d4611c3540da430ad9c423ac2ac77b98b408ad
Kunal Katarya <kunal.rox@gmail.com>
2025-03-11 14:32:29 +0800

    register user E2E but not working. Use as case study

--- src/app.js
+++ src/app.js
@@ -24,8 +24,11 @@ app.use(cookieParser());
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
+import { errorHandler } from "./middleware/error.middlewares.js";
 // all routes
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("api/v1/users", userRouter);
+
+app.use(errorHandler);
 export { app };
